c9e1d8a4b1b6868885d1480d8fd96a6b090cb581
LeonAdeoye <leon.adeoye@gmail.com>
2013-09-06 22:13:45 +0900

    added BeforeClass attributed junit method

--- src/test/SearchingTest.java
+++ src/test/SearchingTest.java
@@ -1,22 +1,56 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import java.util.List;
+
 import junit.framework.TestCase;
-import com.leon.ws.rfq.search.*;
+
 import org.junit.Before; 
+import org.junit.BeforeClass;
+
+import com.leon.ws.rfq.search.SearchController;
+import com.leon.ws.rfq.search.SearchCriterion;
 
 public class SearchingTest extends TestCase
 {
+	private static Logger logger = LoggerFactory.getLogger(SearchingTest.class);
 	private SearchController searcher;
 	
 	public SearchingTest(String name)
-	{
+	{		
 		super(name);
+		System.out.println("XXXX1");
 	}
+	
+	@BeforeClass
+	public static void oneTimeSetup()
+	{
+		System.out.println("XXXX2");
+		try
+		{
+			if(logger.isDebugEnabled())
+				logger.debug("application context class path = " +  System.getProperty("java.class.path"));
+			
+			//ApplicationContext context = new ClassPathXmlApplicationContext("cxf-servlet.xml"); 
+			//searcher = (SearchController) context.getBean("searchController");
+			
+			if(logger.isDebugEnabled())
+				logger.debug("Successfully wired bean search controller from class path application context.");			
+		}
+		catch(BeansException be)
+		{
+			logger.error("failed to load class path application context ", be);
+		}
+	}	
 	
 	@Before
 	public void setUp()
 	{
+		System.out.println("XXXX3 " + System.getProperty("java.class.path"));
 		// TODO switch to relative path
 		ApplicationContext context = new FileSystemXmlApplicationContext("C:\\development\\git\\RequestForQuoteWebService\\src\\main\\webapp\\WEB-INF\\cxf-servlet.xml");
 		searcher = (SearchController) context.getBean("searchController");
@@ -24,7 +58,12 @@ public class SearchingTest extends TestCase
 	
 	public void test_saveAndDelete_addTwoValidCriterionSameOwnerSameKey_twoCriteriaShouldBeAddedWithSameOwnerAndKey()
 	{
+		System.out.println("XXXX4");
+		
 		searcher.save("bob", "bob", "bob", "bob", true, true);
+		
+		System.out.println("XXXX5");
+		
 		searcher.save("bob", "bob", "ethan", "ethan", true, true);
 		
 		List<SearchCriterion> startList = searcher.get("bob", "bob");
