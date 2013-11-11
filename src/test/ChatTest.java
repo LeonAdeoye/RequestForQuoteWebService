25f080eac61bc6e3c66e7188a7697332b68b2dcf
LeonAdeoye <leon.adeoye@gmail.com>
2013-11-12 00:28:08 +0900

    Added Report controller test cases

--- src/test/ChatTest.java
+++ src/test/ChatTest.java
@@ -1,3 +1,5 @@
+import junit.framework.TestCase;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -6,8 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import com.leon.ws.rfq.chat.ChatMediator;
-import junit.framework.TestCase;
 
 public class ChatTest extends TestCase
 {
@@ -19,49 +21,51 @@ public class ChatTest extends TestCase
 		super(name);
 		initializeBean();
 	}
-	
+
 	private void initializeBean()
 	{
 		try
-		{			
-			ApplicationContext context = new FileSystemXmlApplicationContext(".\\src\\main\\webapp\\WEB-INF\\cxf-servlet.xml"); 
-			chatMediator = (ChatMediator) context.getBean("chatMediator");
-			
+		{
+			ApplicationContext context = new FileSystemXmlApplicationContext(".\\src\\main\\webapp\\WEB-INF\\cxf-servlet.xml");
+			this.chatMediator = (ChatMediator) context.getBean("chatMediator");
+
 			if(logger.isDebugEnabled())
-				logger.debug("Successfully wired bean chat mediator from file system application context.");			
+				logger.debug("Successfully wired bean chat mediator from file system application context.");
 		}
 		catch(BeansException be)
 		{
 			logger.error("Failed to load application context for chat mediator!", be);
 		}
-	}		
-	
+	}
+
+	@Override
 	@Before
 	public void setUp()
 	{
 	}
-	
+
 	@Test
 	public void test_sendMessage_RegisteredAndwithValidOwnerAndContent_messageSavedToDatabase()
 	{
-		int previousCount = chatMediator.registerParticipant(99999, "TESTER").getChatMessageList().size();
-		chatMediator.sendMessage(99999, "TESTER", "Test message");
-		int latestCount = chatMediator.getChatMessages(99999, 0).getChatMessageList().size();
-		assertEquals("Test chat message not saved for requestForQuoueId = 99999", previousCount+1, latestCount);	
+		int previousCount = this.chatMediator.registerParticipant(99999, "TESTER").getChatMessageList().size();
+		this.chatMediator.sendMessage(99999, "TESTER", "Test message");
+		int latestCount = this.chatMediator.getChatMessages(99999, 0).getChatMessageList().size();
+		assertEquals("Test chat message not saved for requestForQuoueId = 99999", previousCount+1, latestCount);
 	}
-	
+
 	@Test
 	public void test_sendMessage_UnregisteredAndwithValidOwnerAndContent_messageSavedToDatabase()
 	{
-		int previousCount = chatMediator.getChatMessages(99999, 0).getChatMessageList().size();
-		chatMediator.sendMessage(99999, "TESTER", "Test message");
-		int latestCount = chatMediator.getChatMessages(99999, 0).getChatMessageList().size();
-		assertEquals("Test chat message not saved for requestForQuoueId = 99999", previousCount, latestCount);	
-	}	
-	
+		int previousCount = this.chatMediator.getChatMessages(99999, 0).getChatMessageList().size();
+		this.chatMediator.sendMessage(99999, "TESTER", "Test message");
+		int latestCount = this.chatMediator.getChatMessages(99999, 0).getChatMessageList().size();
+		assertEquals("Test chat message not saved for requestForQuoueId = 99999", previousCount, latestCount);
+	}
+
+	@Override
 	@After
 	public void tearDown()
 	{
-			
-	}	
+
+	}
 }
--- src/test/ReportTest.java
+++ src/test/ReportTest.java
@@ -1,4 +1,59 @@
-public class ReportTest
-{
+import java.util.GregorianCalendar;
+import java.util.List;
 
+import org.junit.Test;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.springframework.util.Assert;
+
+import com.leon.ws.rfq.reporting.ReportingController;
+import com.leon.ws.rfq.reporting.RequestCountReportDataImpl;
+
+//@RunWith(Parameterized.class)
+public class ReportTest extends AbstractDependencyInjectionSpringContextTests
+{
+	@Override
+	protected String[] getConfigLocations()
+	{
+		return new String[] {"file:src\\main\\webapp\\WEB-INF\\cxf-servlet.xml"};
+	}
+
+	//@Parameters({"Client,2013-01-01T00:00:00,0","Status,2013-01-01T00:00:00,0","TradeDate,2013-01-01T00:00:00,0"})
+	@Test
+	public void test_getRequestsByCategory_ClientCategory_ResultReturned()
+	{
+		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("Client", new GregorianCalendar(2013,10,1), 0);
+		Assert.isTrue(result.size() > 1);
+	}
+
+	@Test
+	public void test_getRequestsByCategory_StatusCategory_ResultReturned()
+	{
+		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("Status", new GregorianCalendar(2013,10,1), 0);
+		Assert.isTrue(result.size() > 1);
+	}
+
+	@Test
+	public void test_getRequestsByCategory_TradeDateCategory_ResultReturned()
+	{
+		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("TradeDate", new GregorianCalendar(2013,10,1), 0);
+		Assert.isTrue(result.size() > 1);
+	}
+
+	@Test
+	public void test_getRequestsByCategory_BookCodeCategory_ResultReturned()
+	{
+		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("BookCode", new GregorianCalendar(2013,10,1), 0);
+		Assert.isTrue(result.size() > 1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getRequestsByCategory_EmptyCategoryType_IllegalArgumentExceptionThrown()
+	{
+		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
+		reportingController.getRequestsByCategory("", new GregorianCalendar(2013,10,1), 0);
+	}
 }
