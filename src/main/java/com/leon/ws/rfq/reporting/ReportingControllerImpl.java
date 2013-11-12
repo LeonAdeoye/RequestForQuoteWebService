3b9743a66fdde3e61e776c347e313c6861e1cd63
LeonAdeoye <leon.adeoye@gmail.com>
2013-11-12 22:51:43 +0900

    Commented report controller class's methods.

--- src/main/java/com/leon/ws/rfq/reporting/ReportingControllerImpl.java
+++ src/main/java/com/leon/ws/rfq/reporting/ReportingControllerImpl.java
@@ -15,8 +15,15 @@ public class ReportingControllerImpl implements ReportingController
 	private final static Logger logger = LoggerFactory.getLogger(ReportingControllerImpl.class);
 	private ReportingManagerDao dao;
 
+	/**
+	 * Default constructor
+	 */
 	public ReportingControllerImpl() {}
 
+	/**
+	 * Default constructor.
+	 * @throws NullPointerException			if ReportingManagerDao is null.
+	 */
 	public void setReportingManagerDao(ReportingManagerDao dao)
 	{
 		if(dao == null)
@@ -25,6 +32,14 @@ public class ReportingControllerImpl implements ReportingController
 		this.dao = dao;
 	}
 
+	/**
+	 * Returns a list of RFQ counts for each category type value.
+	 *
+	 * @param categoryType					the category type for each count: BookCode, TradeDate, Client, Status, etc.
+	 * @param fromDate						the trade date of the RFQs to be included in the report.
+	 * @throws IllegalArgumentException		if the categoryType is empty or the minimumCount is < 0.
+	 * @throws NullPointerException			if GregorianCalendar fromDate is null.
+	 */
 	@Override
 	@WebMethod
 	public List<RequestCountReportDataImpl> getRequestsByCategory(String categoryType, GregorianCalendar fromDate, int minimumCount)
@@ -36,7 +51,7 @@ public class ReportingControllerImpl implements ReportingController
 			throw new IllegalArgumentException("minimumCount");
 
 		if(fromDate == null)
-			throw new IllegalArgumentException("fromDate");
+			throw new NullPointerException("fromDate");
 
 		if(logger.isDebugEnabled())
 			logger.debug("Received reporting data request for category: " + categoryType +
--- src/test/ReportTest.java
+++ src/test/ReportTest.java
@@ -1,59 +1,108 @@
+import java.util.Arrays;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.util.Assert;
 
 import com.leon.ws.rfq.reporting.ReportingController;
 import com.leon.ws.rfq.reporting.RequestCountReportDataImpl;
 
-//@RunWith(Parameterized.class)
-public class ReportTest extends AbstractDependencyInjectionSpringContextTests
+@RunWith(value = Parameterized.class)
+public class ReportTest
 {
-	@Override
-	protected String[] getConfigLocations()
+	private static ReportingController reportingController;
+	private final String categoryType;
+	private final GregorianCalendar fromDate;
+	private final int minimumCount;
+
+	public ReportTest(String categoryType, GregorianCalendar fromDate, int minimumCount)
 	{
-		return new String[] {"file:src\\main\\webapp\\WEB-INF\\cxf-servlet.xml"};
+		this.categoryType = categoryType;
+		this.fromDate = fromDate;
+		this.minimumCount = minimumCount;
 	}
 
-	//@Parameters({"Client,2013-01-01T00:00:00,0","Status,2013-01-01T00:00:00,0","TradeDate,2013-01-01T00:00:00,0"})
-	@Test
-	public void test_getRequestsByCategory_ClientCategory_ResultReturned()
+	@Parameters
+	public static Iterable<Object []> data()
 	{
-		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
-		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("Client", new GregorianCalendar(2013,10,1), 0);
+		return Arrays.asList(new Object[][]
+				{
+				{"Client", new GregorianCalendar(2013,10,1), 0},
+				{"Status", new GregorianCalendar(2013,10,1), 0},
+				{"TradeDate", new GregorianCalendar(2013,10,1), 0},
+				{"BookCode", new GregorianCalendar(2013,10,1), 0}
+				});
+	}
+
+	@BeforeClass
+	public static void oneTimeSetUp()
+	{
+		// Arrange
+		ApplicationContext context = new FileSystemXmlApplicationContext(".\\src\\main\\webapp\\WEB-INF\\cxf-servlet.xml");
+		reportingController = (ReportingController) context.getBean("reportingController");
+	}
+
+	@Test
+	public void test_getRequestsByCategory_VariousInput_ValidResultsShouldBeReturned()
+	{
+		// Act
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory(this.categoryType, this.fromDate, this.minimumCount);
+		// Assert
 		Assert.isTrue(result.size() > 1);
 	}
 
 	@Test
-	public void test_getRequestsByCategory_StatusCategory_ResultReturned()
+	public void test_getRequestsByCategory_InvalidCategoryType_NoResultsShouldBeReturned()
 	{
-		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
-		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("Status", new GregorianCalendar(2013,10,1), 0);
-		Assert.isTrue(result.size() > 1);
+		// Act
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("Invalid Category", this.fromDate, this.minimumCount);
+		// Assert
+		Assert.isTrue(result.size() == 0);
 	}
 
 	@Test
-	public void test_getRequestsByCategory_TradeDateCategory_ResultReturned()
+	public void test_getRequestsByCategory_FutureDate_NoResultsShouldBeReturned()
 	{
-		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
-		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("TradeDate", new GregorianCalendar(2013,10,1), 0);
-		Assert.isTrue(result.size() > 1);
+		// Act
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory(this.categoryType, new GregorianCalendar(2020,10,1), this.minimumCount);
+		// Assert
+		Assert.isTrue(result.size() == 0);
 	}
 
 	@Test
-	public void test_getRequestsByCategory_BookCodeCategory_ResultReturned()
+	public void test_getRequestsByCategory_MaxValueMinimumCount_NoResultsShouldBeReturned()
 	{
-		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
-		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory("BookCode", new GregorianCalendar(2013,10,1), 0);
-		Assert.isTrue(result.size() > 1);
+		// Act
+		List<RequestCountReportDataImpl> result = reportingController.getRequestsByCategory(this.categoryType, this.fromDate, Integer.MAX_VALUE);
+		// Assert
+		Assert.isTrue(result.size() == 0);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void test_getRequestsByCategory_NullCalendar_IllegalArgumentExceptionShouldBeThrown()
+	{
+		// Act
+		reportingController.getRequestsByCategory(this.categoryType, null, this.minimumCount);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void test_getRequestsByCategory_EmptyCategoryType_IllegalArgumentExceptionThrown()
+	public void test_getRequestsByCategory_EmptyCategoryType_IllegalArgumentExceptionShouldBeThrown()
 	{
-		ReportingController reportingController = (ReportingController) this.applicationContext.getBean("reportingController");
-		reportingController.getRequestsByCategory("", new GregorianCalendar(2013,10,1), 0);
+		// Act
+		reportingController.getRequestsByCategory("", this.fromDate, this.minimumCount);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getRequestsByCategory_NegativeMinimumCount_IllegalArgumentExceptionShouldBeThrown()
+	{
+		// Act
+		reportingController.getRequestsByCategory(this.categoryType, this.fromDate, -1);
 	}
 }
