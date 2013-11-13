760f47e0baed09be490f27e3e74eae71000dc252
LeonAdeoye <leon.adeoye@gmail.com>
2013-11-14 00:30:26 +0900

    Added missing commentary to the book controller class.

--- src/main/java/com/leon/ws/rfq/book/BookControllerImpl.java
+++ src/main/java/com/leon/ws/rfq/book/BookControllerImpl.java
@@ -20,6 +20,12 @@ public final class BookControllerImpl implements BookController, ApplicationEven
 	private ApplicationEventPublisher applicationEventPublisher;
 	private BookManagerDao dao;
 
+	/**
+	 * Sets the Book Manager DAO object reference property.
+	 * 
+	 * @param dao 						the book manager dao for saving to the database.
+	 * @throws NullPointerException 	if the dao parameter is null.
+	 */
 	@Override
 	@WebMethod(exclude = true)
 	public void setBookManagerDao(BookManagerDao dao)
@@ -30,6 +36,12 @@ public final class BookControllerImpl implements BookController, ApplicationEven
 		this.dao = dao;
 	}
 
+	/**
+	 * Sets the application event publisher.
+	 * 
+	 * @param applicationEventPublisher 	the applicationEventPublisher for publishing events.
+	 * @throws NullPointerException 		if the applicationEventPublisher parameter is null.
+	 */
 	@Override
 	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher)
 	{
@@ -39,8 +51,19 @@ public final class BookControllerImpl implements BookController, ApplicationEven
 		this.applicationEventPublisher = applicationEventPublisher;
 	}
 
+	/**
+	 * Default constructor.
+	 */
 	public BookControllerImpl() {}
 
+
+	/**
+	 * Deletes the book from the database.
+	 * 
+	 * @param bookCode 						the bookCode to be deleted.
+	 * @returns	true if the delete was successful; false otherwise.
+	 * @throws IllegalArgumentException 	if the bookCode parameter is an empty string.
+	 */
 	@Override
 	@WebMethod
 	public boolean delete(String bookCode)
@@ -54,6 +77,15 @@ public final class BookControllerImpl implements BookController, ApplicationEven
 		return this.dao.delete(bookCode);
 	}
 
+	/**
+	 * Saves the book to the database and publishes an event for the listening client communicator.
+	 * 
+	 * @param bookCode 						the bookCode of the book to be saved.
+	 * @param entity						the entity of the book to be saved.
+	 * @param savedBy						the user who is saving the book.
+	 * @returns	true if the save was successful; false otherwise.
+	 * @throws IllegalArgumentException 	if the bookCode or entity or savedBy parameter is an empty string.
+	 */
 	@Override
 	@WebMethod
 	public boolean save(String bookCode, String entity, String savedBy)
@@ -78,6 +110,14 @@ public final class BookControllerImpl implements BookController, ApplicationEven
 		return (newBook != null);
 	}
 
+	/**
+	 * Updates the validity of the book in the database.
+	 * 
+	 * @param bookCode 						the bookCode of the book to be updated.
+	 * @param isValid						the validity of the book to be updated.
+	 * @returns	true if the update was successful; false otherwise.
+	 * @throws IllegalArgumentException 	if the bookCode parameter is an empty string.
+	 */
 	@Override
 	@WebMethod
 	public boolean updateValidity(String bookCode, boolean isValid)
@@ -91,6 +131,10 @@ public final class BookControllerImpl implements BookController, ApplicationEven
 		return this.dao.updateValidity(bookCode, isValid);
 	}
 
+	/**
+	 * Gets all books previously saved to the database.
+	 * @returns a list of books that were previously saved in the database.
+	 */
 	@Override
 	@WebMethod
 	public List<BookDetailImpl> getAll()
