c1b56a2bdea7d7733057c86c9f422c2353eee984
LeonAdeoye <leon.adeoye@gmail.com>
2013-12-04 16:45:18 +0900

    Working transactional management of holiday test database activities

--- src/test/HolidayTest.java
+++ src/test/HolidayTest.java
@@ -1,70 +1,60 @@
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import junit.framework.TestCase;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.leon.ws.rfq.holiday.HolidayController;
 import com.leon.ws.rfq.holiday.HolidayImpl;
 
-public class HolidayTest extends TestCase
+@ContextConfiguration(locations = { "classpath:/cxf-servlet-test.xml" })
+@Transactional
+public class HolidayTest extends AbstractJUnit4SpringContextTests
 {
-	private static final Logger logger = LoggerFactory.getLogger(HolidayTest.class);
+	@Autowired
 	private HolidayController holidayController;
+	
+	@Autowired
+	private DataSourceTransactionManager transactionManager;
+	
+	private TransactionStatus status;
 
-	public HolidayTest(String name)
-	{
-		super(name);
-		initializeBean();
-	}
-
-	private void initializeBean()
-	{
-		try
-		{
-			ApplicationContext context = new FileSystemXmlApplicationContext(".\\src\\main\\webapp\\WEB-INF\\cxf-servlet.xml");
-			this.holidayController = (HolidayController) context.getBean("holidayController");
-
-			if(logger.isDebugEnabled())
-				logger.debug("Successfully wired bean holiday controller from file system application context.");
-		}
-		catch(BeansException be)
-		{
-			logger.error("Failed to load application context for holiday controller!", be);
-		}
-	}
-
-	@Override
-	@Before
-	public void setUp()
-	{
-	}
+	public HolidayTest() {}
 
 	@Test
-	public void test_addOneValidHolidayToLiverpool_LiverpoolHolidaysIncrementedByOne()
+	@Rollback(true)
+	public void test_addOneValidHoliday_HolidaysIncrementedByOne()
 	{
-		this.holidayController.delete("TEST_LOCATION", "23 Dec 2013");
-		
 		this.holidayController.save("TEST_LOCATION", "23 Dec 2013", "testuser");
 		List<HolidayImpl> after = this.holidayController.get("TEST_LOCATION");
 		assertEquals("Test holiday not saved for TEST_LOCATION", 1, after.size());
-
-		this.holidayController.delete("TEST_LOCATION", "23 Dec 2013");
-		after = this.holidayController.get("TEST_LOCATION");
-		assertEquals("Test holiday not deleted for TEST_LOCATION", 0, after.size());
 	}
-
-	@Override
+	
+	@Before
+	public void setUp()
+	{
+		assertNotNull("autowired holiday controller should not be null", this.holidayController);
+		assertNotNull("autowired transaction manager should not be null", this.transactionManager);
+		
+		TransactionDefinition paramTransactionDefinition = new DefaultTransactionDefinition();
+		this.status=this.transactionManager.getTransaction(paramTransactionDefinition);
+	}
+	
 	@After
 	public void tearDown()
 	{
-
+		this.transactionManager.rollback(this.status);
 	}
 }
