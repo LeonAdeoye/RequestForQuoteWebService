03928471a4bcd867edfa7668270cbd00f1095e5d
LeonAdeoye <leon.adeoye@gmail.com>
2014-01-01 23:12:38 +0000

    Improved test cases to be able to save

--- src/test/RequestManagerDaoTest.java
+++ src/test/RequestManagerDaoTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
+
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.After;
 import org.junit.Before;
@@ -14,6 +16,8 @@ import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import com.leon.ws.rfq.request.OptionDetailImpl;
+import com.leon.ws.rfq.request.OptionDetailListImpl;
 import com.leon.ws.rfq.request.RequestDetailImpl;
 import com.leon.ws.rfq.request.RequestManagerDao;
 
@@ -30,7 +34,10 @@ public class RequestManagerDaoTest  extends AbstractJUnit4SpringContextTests
 	private DataSourceTransactionManager transactionManager;
 	
 	private TransactionStatus status;
-
+	
+	private final OptionDetailListImpl optionLegs = new OptionDetailListImpl();
+	private final RequestDetailImpl request = new RequestDetailImpl();
+	
 	public RequestManagerDaoTest() {}
 	
 	@Before
@@ -41,6 +48,71 @@ public class RequestManagerDaoTest  extends AbstractJUnit4SpringContextTests
 		
 		TransactionDefinition paramTransactionDefinition = new DefaultTransactionDefinition();
 		this.status=this.transactionManager.getTransaction(paramTransactionDefinition);
+		
+		this.optionLegs.setOptionDetailList(new ArrayList<OptionDetailImpl>());
+		
+		this.request.setLegs(this.optionLegs);
+		this.request.setQuantity(1);
+		this.request.setAskFinalAmount(1.0);
+		this.request.setAskFinalPercentage(2.0);
+		this.request.setAskImpliedVol(3.0);
+		this.request.setAskPremiumAmount(4.0);
+		this.request.setAskPremiumPercentage(5.0);
+		this.request.setBidFinalAmount(6.0);
+		this.request.setBidFinalPercentage(7.0);
+		this.request.setBidPremiumAmount(8.0);
+		this.request.setBidPremiumPercentage(9.0);
+		this.request.setBookCode("AB01");
+		this.request.setClientComment("test comment");
+		this.request.setClientId(1);
+		this.request.setContracts(2);
+		this.request.setDelta(10.0);
+		this.request.setDeltaNotional(11.0);
+		this.request.setDeltaShares(12.0);
+		this.request.setExpiryDate("12/23/2014");
+		this.request.setGamma(13.0);
+		this.request.setGammaNotional(14.0);
+		this.request.setGammaShares(15.0);
+		this.request.setHedgePrice(16.0);
+		this.request.setHedgeType("Test");
+		this.request.setImpliedVol(17.0);
+		this.request.setInterestRate(18.0);
+		this.request.setIsOTC(true);
+		this.request.setLotSize(3);
+		this.request.setMultiplier(4);
+		this.request.setNotionalCurrency("USD");
+		this.request.setNotionalFXRate(19.0);
+		this.request.setNotionalMillions(20.0);
+		this.request.setPickedUpBy("test");
+		this.request.setPremiumAmount(21.0);
+		this.request.setPremiumPercentage(22.0);
+		this.request.setPremiumSettlementCurrency("USD");
+		this.request.setPremiumSettlementDate("12/23/2014");
+		this.request.setPremiumSettlementDaysOverride(5);
+		this.request.setPremiumSettlementFXRate(23.0);
+		this.request.setRequest("test");
+		this.request.setRho(24.0);
+		this.request.setRhoNotional(25.0);
+		this.request.setRhoShares(26.0);
+		this.request.setSalesComment("test");
+		this.request.setSalesCreditAmount(27.0);
+		this.request.setSalesCreditCurrency("USD");
+		this.request.setSalesCreditFXRate(28.0);
+		this.request.setSalesCreditPercentage(29.0);
+		this.request.setStatus("test");
+		this.request.setStrike(30.0);
+		this.request.setTheta(31.0);
+		this.request.setThetaNotional(32.0);
+		this.request.setThetaShares(33.0);
+		this.request.setTimeToExpiry(34.0);
+		this.request.setTotalPremium(35.0);
+		this.request.setTradeDate("12/23/2013");
+		this.request.setTraderComment("test");
+		this.request.setUnderlyingPrice(100);
+		this.request.setVega(36.0);
+		this.request.setVegaNotional(37.0);
+		this.request.setVegaShares(38.0);
+		this.request.setVolatility(39.0);
 	}
 	
 	@After
@@ -77,66 +149,63 @@ public class RequestManagerDaoTest  extends AbstractJUnit4SpringContextTests
 	@Test(expected = IllegalArgumentException.class)
 	public void test_save_NullTradeDate_ExceptionTrown()
 	{
-		RequestDetailImpl request = new RequestDetailImpl();
-		request.setRequest("test");
-		request.setTradeDate(null);
-		request.setExpiryDate("12/23/2014");
-		request.setPremiumSettlementDate("12/23/2014");
-		this.requestManagerDao.save(request, "testUser");
+		// Arrange
+		this.request.setTradeDate(null);
+		// Act
+		this.requestManagerDao.save(this.request, "testUser");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_save_InvalidTradeDate_ExceptionTrown()
 	{
-		RequestDetailImpl request = new RequestDetailImpl();
-		request.setRequest("test");
-		request.setTradeDate("");
-		request.setExpiryDate("12/23/2014");
-		request.setPremiumSettlementDate("12/23/2014");
-		this.requestManagerDao.save(request, "testUser");
+		// Arrange
+		this.request.setTradeDate("");
+		// Act
+		this.requestManagerDao.save(this.request, "testUser");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_save_NullExpiryDate_ExceptionTrown()
 	{
-		RequestDetailImpl request = new RequestDetailImpl();
-		request.setRequest("test");
-		request.setTradeDate("12/23/2013");
-		request.setExpiryDate(null);
-		request.setPremiumSettlementDate("12/23/2014");
-		this.requestManagerDao.save(request, "testUser");
+		// Arrange
+		this.request.setExpiryDate(null);
+		// Act
+		this.requestManagerDao.save(this.request, "testUser");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_save_InvalidExpiryDate_ExceptionTrown()
 	{
-		RequestDetailImpl request = new RequestDetailImpl();
-		request.setRequest("test");
-		request.setTradeDate("12/23/2013");
-		request.setExpiryDate("");
-		request.setPremiumSettlementDate("12/23/2014");
-		this.requestManagerDao.save(request, "testUser");
+		// Arrange
+		this.request.setExpiryDate("");
+		// Act
+		this.requestManagerDao.save(this.request, "testUser");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_save_NullPremiumSettlementDate_ExceptionTrown()
 	{
-		RequestDetailImpl request = new RequestDetailImpl();
-		request.setRequest("test");
-		request.setTradeDate("12/23/2013");
-		request.setExpiryDate("12/23/2014");
-		request.setPremiumSettlementDate(null);
-		this.requestManagerDao.save(request, "testUser");
+		// Arrange
+		this.request.setPremiumSettlementDate(null);
+		// Act
+		this.requestManagerDao.save(this.request, "testUser");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_save_InvalidPremiumSettlementDate_ExceptionTrown()
 	{
-		RequestDetailImpl request = new RequestDetailImpl();
-		request.setRequest("test");
-		request.setTradeDate("12/23/2013");
-		request.setExpiryDate("12/23/2014");
-		request.setPremiumSettlementDate("");
-		this.requestManagerDao.save(request, "testUser");
+		// Arrange
+		this.request.setPremiumSettlementDate("");
+		// Act
+		this.requestManagerDao.save(this.request, "testUser");
+	}
+	
+	@Test
+	public void test_save_CompleteRequest_ValidRequestReturned()
+	{
+		// Act
+		RequestDetailImpl result = this.requestManagerDao.save(this.request, "testUser");
+		// Assert
+		assertNotNull("request should NOT be null", result);
 	}
 }
