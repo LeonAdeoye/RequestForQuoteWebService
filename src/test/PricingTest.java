a97085c6fce49dff8f198222768cb1e0cfbfad4a
LeonAdeoye <leon.adeoye@gmail.com>
2014-01-16 23:52:31 +0000

    More pricing unit tests improvements

--- src/test/PricingTest.java
+++ src/test/PricingTest.java
@@ -111,7 +111,7 @@ public class PricingTest extends AbstractJUnit4SpringContextTests
 	@Test(expected = IllegalArgumentException.class)
 	public void test_calculate_ZeroVolatility_IllegalArgumentExceptionThrown()
 	{
-		this.pricingController.calculate(this.strike,0, this.underlyingPrice,
+		this.pricingController.calculate(this.strike, 0, this.underlyingPrice,
 				this.daysToExpiry, this.interestRate, true, true, this.dayCountConvention);
 	}
 	
@@ -134,6 +134,13 @@ public class PricingTest extends AbstractJUnit4SpringContextTests
 	{
 		this.pricingController.calculate(this.strike,this.volatility, this.underlyingPrice,
 				Double.NEGATIVE_INFINITY, this.interestRate, true, true, this.dayCountConvention);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_calculate_ZeroDaysToExpiry_IllegalArgumentExceptionThrown()
+	{
+		this.pricingController.calculate(this.strike,this.volatility, this.underlyingPrice,
+				0, this.interestRate, true, true, this.dayCountConvention);
 	}
 	
 	@Test
